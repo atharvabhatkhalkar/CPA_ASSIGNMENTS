e0aa96b1a293def9731bc7f27fbec27f88be9093
atharvabhatkhalkar <42717901+atharvabhatkhalkar@users.noreply.github.com>
2021-07-10 17:42:45 +0530

    Looping_002

--- Looping_001.cpp
+++ Looping_001.cpp
@@ -1,31 +1,37 @@
 #include <stdio.h> 
 #pragma warning(disable:4996)
 
-// for - summations 
+// looping subtraction 
 int main(void)
 {
     int i;
-    int n=0;
-    int k=1;
+    int n;
+    int k;
 
     puts("block 1");
-    for (i = 0; i < 5; ++i)
+    for (i = 5; i > 0; i--)
         printf("i = %d\n", i);
 
     puts("block 2");
-    for (i = 0; i <= 10; i = i + 2)
+    for (i = 5; i >= 0; --i)
         printf("i = %d\n", i);
 
     puts("block 3");
-    for (i = 1; i <= 10; i += 2)
+    for (i = 10; i >= 0; i -= 2)
         printf("i = %d\n", i);
 
     puts("block 4");
-    printf("Enter n:");
-    scanf_s("%d",&n);
+    for (i = 10; i > 0; i = i - 2)
+        printf("i = %d\n", i);
 
+    puts("block 5");
+    printf("Enter n:");
+    scanf("%d", &n);
     printf("Enter k:");
-    for (i = 0; i < n; i += k)
+    scanf("%d", &k);
+    for (i = n; i > 0; i = i - k)
+        printf("i = %d\n", i);
+    for (i = n; i >= 0; i -= k)
         printf("i = %d\n", i);
 
     return (0);
