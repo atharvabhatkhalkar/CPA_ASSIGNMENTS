012aa7945c73cb4938228c6548851b8fb0922014
atharvabhatkhalkar <42717901+atharvabhatkhalkar@users.noreply.github.com>
2021-07-10 18:12:50 +0530

    Looping_007
    level_2 nested loops

---
+++ Looping_007.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h> 
+#pragma warning(disable:4996)
+
+
+// nested loops  : level 2 
+int main(void)
+{
+    int i, j;
+    int m = 8, n = 7;
+
+    puts("block 1");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < m; ++j)
+        {
+            printf("i = %d, j = %d\n", i, j);
+        }
+    }
+
+    puts("block 2");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            if (i < j)
+                printf("i = %d, j = %d\n", i, j);
+        }
+    }
+
+    puts("block 3");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            if (i <= j)
+            {
+                printf("i = %d j = %d\n", i, j);
+            }
+        }
+    }
+
+    puts("block 4");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            if (i > j)
+            {
+                printf("i = %d j = %d\n", i, j);
+            }
+        }
+    }
+
+    puts("block 5");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            if (i >= j)
+            {
+                printf("i = %d j = %d\n", i, j);
+            }
+        }
+    }
+
+    puts("block 6");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            if (i == j)
+                printf("i = %d j = %d\n", i, j);
+        }
+    }
+
+    puts("block 7");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            if (i != j)
+            {
+                printf("i = %d j = %d\n", i, j);
+            }
+        }
+    }
+
+    return (0);
+}
